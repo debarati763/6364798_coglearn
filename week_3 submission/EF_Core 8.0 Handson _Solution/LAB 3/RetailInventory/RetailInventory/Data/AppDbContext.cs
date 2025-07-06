068b8891792ae5bde499efd461d529746d010b4b
debarati763 <debaratibanerjee763@gmail.com>
2025-07-06 23:58:51 +0530

    add week 3 handson solution

---
+++ week_3 submission/EF_Core 8.0 Handson _Solution/LAB 3/RetailInventory/RetailInventory/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory.Models;
+
+namespace RetailInventoryApp.Data
+
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=RetailInventoryDb;Trusted_Connection=True;Encrypt=False;");
+
+
+
+        }
+    }
+}
---
+++ week_3 submission/EF_Core 8.0 Handson _Solution/LAB 5/RetailInventory/RetailInventory/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventoryApp.Data;
+using RetailInventory.Models;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using var context = new AppDbContext();
+
+        Console.WriteLine("== All Products ==");
+        var products = await context.Products.ToListAsync();
+        foreach (var p in products)
+            Console.WriteLine($"{p.Name} - ₹{p.Price}");
+
+        Console.WriteLine("\n== Find by ID ==");
+        var product = await context.Products.FindAsync(1);
+        Console.WriteLine($"Found: {product?.Name}");
+
+        Console.WriteLine("\n== First Product > ₹50,000 ==");
+        var expensive = await context.Products.FirstOrDefaultAsync(p => p.Price > 50000);
+        Console.WriteLine($"Expensive: {expensive?.Name}");
+    }
+}
